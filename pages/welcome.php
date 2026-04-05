a0611a5e5f778e74e7f80ef8882f758098093fa8
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2026-04-06 00:42:45 +0300

    welcome fixes

---
+++ pages/welcome.php
@@ -0,0 +1,13 @@
+<div class="welcome">
+    <div class="logo"><span>Power</span><span style="color: var(--text)">Calculator</span></div>
+        
+    <div class="welcome-description">
+        <p>Сплануйте свою систему живлення за лічені хвилини:</p>
+        <ul class="welcome-features">
+            <li><strong>Аналіз АКБ:</strong> Розрахунок реальної ємності з урахуванням DoD та температури.</li>
+            <li><strong>Моніторинг навантаження:</strong> Облік пускових струмів та активної потужності приладів.</li>
+            <li><strong>Прогноз автономності:</strong> Точний час роботи вашої оселі від одного заряду.</li>
+        </ul>
+        <p class="welcome-call">Авторизуйтесь, щоб зберегти власну конфігурацію приладів</p>
+    </div>
+</div>
