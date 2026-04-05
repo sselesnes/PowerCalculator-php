29e741a05556252fb2da7879bf8dc380ebc344fd
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2026-04-05 17:24:00 +0300

    Delete includes/menu.php

--- includes/menu.php
+++
@@ -1,7 +0,0 @@
-<div id="menu">
-<ul>
- <li><a href="/?page=overview" title="Overview">Overview</a></li>
- <li><a href="/?page=calculate" title="Calculate">Calculate</a></li>
- <li><a href="/?page=contact" title="Contact us">Contact us</a></li>
-</ul>
-</div>
